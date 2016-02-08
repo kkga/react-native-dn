473fd78fbe8efc5bd33a12b13ef2af9b59976b5c
Gadzhi Kharkharov <kkga@me.com>
2016-02-08 14:31:29 +0300

    design story item

--- App/Components/Story.js
+++ App/Components/Story.js
@@ -9,14 +9,28 @@ import React, {
 
 class Story extends Component {
   render() {
+    var topStory = this.props.vote_count > 10;
+
     return (
-      <TouchableHighlight
-        style={styles.row}
-        onPress={this.props.onPress}>
-        <View>
-          <Text style={styles.title}>{this.props.title}</Text>
-          <Text style={styles.votes}>{this.props.vote_count}</Text>
-          <Text style={styles.hostname}>{this.props.hostname}</Text>
+      <TouchableHighlight onPress={this.props.onPress}>
+        <View style={styles.container}>
+          <View style={styles.voteContainer}>
+            <View style={topStory ? styles.voteLabelTop : styles.voteLabel}>
+              <Text style={styles.voteCount}>
+                {this.props.vote_count}
+              </Text>
+            </View>
+          </View>
+
+          <View style={styles.infoContainer}>
+            <Text style={styles.title}>{this.props.title}</Text>
+
+            <View style={styles.meta}>
+              {this.props.hostname &&
+                <Text style={styles.source}>{this.props.hostname.toUpperCase()}</Text>
+              }
+            </View>
+          </View>
         </View>
       </TouchableHighlight>
     );
@@ -24,13 +38,49 @@ class Story extends Component {
 }
 
 var styles = StyleSheet.create({
-  row: {
-    paddingHorizontal: 15,
-    paddingVertical: 10,
+  container: {
+    flexDirection: 'row',
+    paddingVertical: 12,
+  },
+  voteContainer: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  infoContainer: {
+    flex: 7,
+  },
+  voteLabel: {
+    paddingHorizontal: 4,
+    paddingVertical: 3,
+    borderRadius: 3,
+    width: 28,
+    backgroundColor: '#EFEFEF'
+  },
+  voteLabelTop: {
+    paddingHorizontal: 4,
+    paddingVertical: 3,
+    borderRadius: 3,
+    width: 28,
+    backgroundColor: 'green'
+  },
+  voteCount: {
+    fontSize: 12,
+    textAlign: 'center',
+    opacity: 0.65,
   },
   title: {
     fontSize: 16,
+    fontWeight: '500',
+    marginBottom: 12,
   },
+  meta: {
+    flexDirection: 'row',
+    opacity: 0.5,
+  },
+  source: {
+    fontSize: 10,
+    fontWeight: '500',
+  }
 });
 
 module.exports = Story;

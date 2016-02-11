9926ece2ae1b92c5214a34966259e45abf367961
Gadzhi Kharkharov <gadzhi.kha@gmail.com>
2016-02-11 20:08:55 +0300

    add refreshcontrol and sorting options

--- src/components/List.js
+++ src/components/List.js
@@ -4,7 +4,10 @@ import React, {
   ListView,
   StyleSheet,
   View,
+  Text,
   ActivityIndicatorIOS,
+  TouchableOpacity,
+  RefreshControl,
 } from 'react-native';
 
 import Story from './Story.js';
@@ -19,33 +22,63 @@ class List extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      isLoading: true,
+      isRefreshing: false,
+      requestUrl: api.DN_TOP,
       dataSource: new ListView.DataSource({
         rowHasChanged: (row1, row2) => row1 !== row2,
       }),
-      loading: true,
     };
     this.renderRow = this.renderRow.bind(this);
-    this.onPress = this.onPress.bind(this);
+    this.onRowPress = this.onRowPress.bind(this);
+    this.onRefresh = this.onRefresh.bind(this);
   }
 
   componentDidMount() {
     this.fetchData();
   }
 
-  onPress(rowData) {
+  onRowPress(rowData) {
     this.props.navigator.push({
       name: 'Details',
       url: rowData.url,
     });
   }
 
+  onChangeSorting(sorting) {
+    switch (sorting) {
+      default:
+      case 'top':
+        this.setState({
+          isLoading: true,
+          requestUrl: api.DN_TOP,
+        });
+        this.fetchData();
+        break;
+      case 'recent':
+        this.setState({
+          isLoading: true,
+          requestUrl: api.DN_RECENT,
+        });
+        this.fetchData();
+    }
+  }
+
+  onRefresh() {
+    this.setState({
+      isRefreshing: true,
+    });
+    this.fetchData();
+  }
+
   fetchData() {
-    fetch(api.DN_STORIES)
+    fetch(this.state.requestUrl)
       .then((response) => response.json())
       .then((responseData) => {
         this.setState({
           dataSource: this.state.dataSource.cloneWithRows(responseData.stories),
-          loading: false,
+          isLoading: false,
+          isRefreshing: false,
         });
       })
       .done();
@@ -59,34 +92,53 @@ class List extends Component {
     );
   }
 
-  renderSeparator() {
+  renderSeparator(rowData, rowID) {
     return (
-      <View style={styles.separator} />
+      <View key={'SEP_' + rowID} style={styles.separator} />
     );
   }
 
-  renderRow(rowData) {
+  renderRow(rowData, rowID) {
     return (
       <Story
         hostname={rowData.hostname}
         title={rowData.title}
+        key={rowID}
+        id={rowData.id}
         vote_count={rowData.vote_count}
-        onPress={() => this.onPress(rowData)}
+        created_at={rowData.created_at}
+        comment_count={rowData.comment_count}
+        onPress={() => this.onRowPress(rowData)}
       />
     );
   }
 
   render() {
-    if (this.state.loading) {
+    if (this.state.isLoading) {
       return this.renderLoadingView();
     }
 
     return (
       <View style={styles.container}>
+        <TouchableOpacity onPress={() => this.onChangeSorting('recent')}>
+          <Text>Show Recent</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => this.onChangeSorting('top')}>
+          <Text>Show Top</Text>
+        </TouchableOpacity>
         <ListView
           dataSource={this.state.dataSource}
           renderRow={this.renderRow}
           renderSeparator={this.renderSeparator}
+          refreshControl={
+            <RefreshControl
+              refreshing={this.state.isRefreshing}
+              onRefresh={this.onRefresh}
+              tintColor="#ff0000"
+              colors={['#ff0000', '#00ff00', '#0000ff']}
+              progressBackgroundColor="#ffff00"
+            />
+          }
         />
       </View>
     );

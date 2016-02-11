be433c198ddec07aaf82fdc9cfd83e7b264164cc
Gadzhi Kharkharov <gadzhi.kha@gmail.com>
2016-02-12 00:51:01 +0300

    add tabbar icons

--- src/components/Home.js
+++ src/components/Home.js
@@ -34,11 +34,13 @@ class Home extends Component {
     return (
       <View style={styles.container}>
         <TabBarIOS
-          tintColor="blue"
+          tintColor="#276FDC"
           barTintColor="white"
         >
           <TabBarIOS.Item
             title="Top Stories"
+            icon={require('../img/top.png')}
+            selectedIcon={require('../img/top-selected.png')}
             selected={this.state.selectedTab === 'topStories'}
             onPress={() => {
               this.setState({
@@ -50,6 +52,8 @@ class Home extends Component {
           </TabBarIOS.Item>
           <TabBarIOS.Item
             title="Recent Stories"
+            icon={require('../img/recent.png')}
+            selectedIcon={require('../img/recent-selected.png')}
             badge={this.state.notifCount > 0 ? this.state.notifCount : undefined}
             selected={this.state.selectedTab === 'recentStories'}
             onPress={() => {
@@ -60,7 +64,36 @@ class Home extends Component {
           >
             {this.renderTab('recentStories')}
           </TabBarIOS.Item>
+          <TabBarIOS.Item
+            title="Discussions"
+            icon={require('../img/discussions.png')}
+            selectedIcon={require('../img/discussions-selected.png')}
+            badge={this.state.notifCount > 0 ? this.state.notifCount : undefined}
+            selected={this.state.selectedTab === 'discussions'}
+            onPress={() => {
+              this.setState({
+                selectedTab: 'discussions',
+              });
+            }}
+          >
+            {this.renderTab('recentStories')}
+          </TabBarIOS.Item>
+          <TabBarIOS.Item
+            title="You"
+            icon={require('../img/you.png')}
+            selectedIcon={require('../img/you-selected.png')}
+            badge={this.state.notifCount > 0 ? this.state.notifCount : undefined}
+            selected={this.state.selectedTab === 'you'}
+            onPress={() => {
+              this.setState({
+                selectedTab: 'you',
+              });
+            }}
+          >
+            {this.renderTab('recentStories')}
+          </TabBarIOS.Item>
         </TabBarIOS>
+
       </View>
     );
   }

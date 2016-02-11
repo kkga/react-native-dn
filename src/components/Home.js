926f03d31debfb145606d44e8503ae95ca1caf01
Gadzhi Kharkharov <gadzhi.kha@gmail.com>
2016-02-11 23:01:58 +0300

    container for statusbar margin

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,8 @@ import React, {
   Component,
   TabBarIOS,
   PropTypes,
+  View,
+  StyleSheet,
 } from 'react-native';
 
 import List from './List';
@@ -30,38 +32,47 @@ class Home extends Component {
 
   render() {
     return (
-      <TabBarIOS
-        tintColor="white"
-        barTintColor="black"
-      >
-        <TabBarIOS.Item
-          systemIcon="favorites"
-          title="Top Stories"
-          selected={this.state.selectedTab === 'topStories'}
-          onPress={() => {
-            this.setState({
-              selectedTab: 'topStories',
-            });
-          }}
+      <View style={styles.container}>
+        <TabBarIOS
+          tintColor="white"
+          barTintColor="black"
         >
-          {this.renderTab('topStories')}
-        </TabBarIOS.Item>
-        <TabBarIOS.Item
-          systemIcon="most-recent"
-          title="Recent Stories"
-          badge={this.state.notifCount > 0 ? this.state.notifCount : undefined}
-          selected={this.state.selectedTab === 'recentStories'}
-          onPress={() => {
-            this.setState({
-              selectedTab: 'recentStories',
-            });
-          }}
-        >
-          {this.renderTab('recentStories')}
-        </TabBarIOS.Item>
-      </TabBarIOS>
+          <TabBarIOS.Item
+            systemIcon="favorites"
+            title="Top Stories"
+            selected={this.state.selectedTab === 'topStories'}
+            onPress={() => {
+              this.setState({
+                selectedTab: 'topStories',
+              });
+            }}
+          >
+            {this.renderTab('topStories')}
+          </TabBarIOS.Item>
+          <TabBarIOS.Item
+            systemIcon="most-recent"
+            title="Recent Stories"
+            badge={this.state.notifCount > 0 ? this.state.notifCount : undefined}
+            selected={this.state.selectedTab === 'recentStories'}
+            onPress={() => {
+              this.setState({
+                selectedTab: 'recentStories',
+              });
+            }}
+          >
+            {this.renderTab('recentStories')}
+          </TabBarIOS.Item>
+        </TabBarIOS>
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 20,
+  },
+});
 
 export default Home;

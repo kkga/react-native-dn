d6256f1d61e4a6626e4c2160dc370907718da07a
Gadzhi Kharkharov <gadzhi.kha@gmail.com>
2016-02-13 02:59:33 +0300

    extract badge component

---
+++ src/components/Badge.js
@@ -0,0 +1,50 @@
+import React, {
+  Component,
+  PropTypes,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+import badgeColors from '../styles/badgeColors';
+
+class Badge extends Component {
+
+  static propTypes = {
+    type: PropTypes.string.isRequired,
+  };
+
+  getColors() {
+    const badge = badgeColors[this.props.type];
+    const colors = { text: badge.text, bg: badge.bg };
+
+    return colors;
+  }
+
+  render() {
+    const colors = this.getColors();
+
+    return (
+      <View style={[styles.container, { backgroundColor: colors.bg }]}>
+        <Text style={[styles.text, { color: colors.text }]}>
+          {this.props.type.toUpperCase()}
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginRight: 6,
+    paddingHorizontal: 4,
+    paddingVertical: 2,
+    borderRadius: 3,
+  },
+  text: {
+    fontSize: 11,
+    fontWeight: '600',
+  },
+});
+
+export default Badge;

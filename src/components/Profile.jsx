507c4ac78a7d11348e7a24b4117ed70f9db3bd13
Gadzhi Kharkharov <gadzhi.kha@gmail.com>
2016-06-24 14:40:18 +0300

    profile login

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -9,26 +9,28 @@ import React, {
 
 class Profile extends Component {
   constructor(props) {
-  super(props);
+    super(props);
 
-  this.promptResponse = this.promptResponse.bind(this);
-  this.state = {
-    promptValue: undefined,
-  };
+    this.promptResponse = this.promptResponse.bind(this);
+    this.state = {
+      promptValue: undefined,
+      token: undefined,
+    };
 
-  this.title = 'Type a value';
-  this.defaultValue = 'Default value';
-  this.buttons = [{
-    text: 'Custom OK',
-    onPress: this.promptResponse
-  }, {
-    text: 'Custom Cancel',
-    style: 'cancel',
-  }];
+    this.myInfo = this.myInfo.bind(this);
+
+    this.title = 'Type a value';
+    this.defaultValue = 'Default value';
+    this.buttons = [{
+      text: 'Custom OK',
+      onPress: this.promptResponse,
+    }, {
+      text: 'Custom Cancel',
+      style: 'cancel',
+    }];
   }
 
   prompt() {
-    // Flow's apply support is broken: #7035621
     ((AlertIOS.prompt: any).apply: any)(AlertIOS, arguments);
   }
 
@@ -36,17 +38,64 @@ class Profile extends Component {
     this.setState({ promptValue });
   }
 
+  componentDidMount() {
+    this.getToken();
+    // this.myInfo();
+  }
+
+  getToken() {
+    const requestObj = {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        client_id: '576c722a2fd70d15a7a3e07d54c49181d2e4e060361590119a0a74be80396972',
+        client_secret: 'c087d9e638de92dbfb17166186b838b302a698fafc13deac465c5bbf4ba190a5',
+        grant_type: 'client_credentials',
+      }),
+    };
+
+    return fetch('https://www.designernews.co/oauth/token', requestObj)
+      .then((res) => res.json())
+      .then((responseData) => {
+        this.setState({
+          token: responseData.access_token,
+        });
+      });
+  }
+
+  myInfo() {
+    const requestUrl = 'https://www.designernews.co/api/v2/me';
+    const token = this.state.token;
+    const requestObj = {
+      headers: {
+        Authorization: 'bearer ' + token,
+      },
+    };
+
+    fetch(requestUrl, requestObj)
+      .then((response) => response.json())
+      .then((responseData) => console.log(responseData))
+      .catch((error) => { console.warn(error); })
+      .done();
+  }
+
   render() {
     return (
       <View style={styles.container}>
 
-      <TouchableHighlight
+        <TouchableHighlight
           style={styles.wrapper}
-          onPress={this.prompt.bind(this, this.title, this.defaultValue, null, this.promptResponse)}>
+          onPress={
+            this.prompt.bind(this, this.title, this.defaultValue, null, this.promptResponse)
+          }
+        >
 
           <View style={styles.button}>
-            <Text>
-              prompt with title, default value & callback
+            <Text onPress={this.myInfo}>
+              get my info
             </Text>
           </View>
         </TouchableHighlight>
